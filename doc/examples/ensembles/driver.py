b63963141b43c13f01c58ce488c9be481d367571
Mark Coletti <colettima@ornl.gov>
2024-12-11 11:58:12 -0500

    Rewrote how variables are supported.

--- doc/examples/ensembles/driver.py
+++ doc/examples/ensembles/driver.py
@@ -5,6 +5,7 @@
 import os
 from ipsframework import Component
 
+
 class ensemble_driver(Component):
 
     def __init__(self):
@@ -15,20 +16,15 @@ class ensemble_driver(Component):
         """
         self.services.stage_state()
 
-        # Define dict of ensemble variables
-        ensemble_variables = {
-            'X0': [1, 2, 3, 4, 5],
-            'a_line': [2.34, 5.82, 0.1],
-            'foo': ['bar', 'baz', 'qux']
-            # will have other means of creating ranges; e.g., generator functions
-        }
-
         # Get the template input file
         ports = self.services.get_global_param('PORTS')
-        template = ['ENSEMBLE']['TEMPLATE']
+        component = ['WORKER']
 
-        # `excludes` is a callback function that will be called for each
-        # ensemble member. If it returns True, the ensemble member will be
-        # excluded from the ensemble.  This is a way for a SME to filter out
-        # combinations that are not valid.
-        self.services.run_ensemble(template, ensemble_variables, excludes=None)
+        # Specifies three sets of variable values for three
+        # concurrent ensemble runs of the WORKER component.
+        variables = [{‘A’: 3, ‘B’: 2.34, ‘C’: ‘bar’},
+        {‘A’: 2, ‘B’: 5.82, ‘C’: ‘baz’},
+        {‘A’: 4, ‘B’: 0.1, ‘C’: ‘quux’}]
+
+        # spins up N tasks, in this case 2
+        self.services.run_ensemble(component, ensemble_variables)
