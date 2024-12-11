0ea533de63a7fd593e401ba31c6f93520e37ac52
Mark Coletti <colettima@ornl.gov>
2024-12-11 16:46:33 -0500

    Checkpoint commit

--- doc/examples/ensembles/driver.py
+++ doc/examples/ensembles/driver.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
 """
-    Example driver for the ensembles example
+    Example driver for the ensembles example.
+
+    Please note that run_ensemble can be run from any IPS component, not
+    just a driver. The driver is used here for simplicity.
 """
 import os
+from timeit import template
+
 from ipsframework import Component
 
 
 class ensemble_driver(Component):
 
-    def __init__(self):
+    def __init__(self, services, config):
         super().__init__(services, config)
 
     def step(self, timestamp=0.0, **keywords):
         """ set up and run the ensemble
         """
-        self.services.stage_state()
+        run_dir = self.config.get_global_param(self, self.services, 'ENSEMBLE_DIR')
 
-        # Get the template input file
-        ports = self.services.get_global_param('PORTS')
-        component = ['WORKER']
+        template = self.config.get_config_param('driver.TEMPLATE')
 
         # Specifies three sets of variable values for three
         # concurrent ensemble runs of the WORKER component.
-        variables = [{‘A’: 3, ‘B’: 2.34, ‘C’: ‘bar’},
-        {‘A’: 2, ‘B’: 5.82, ‘C’: ‘baz’},
-        {‘A’: 4, ‘B’: 0.1, ‘C’: ‘quux’}]
+        variables = {'orange': {'A': [3, 2, 4],'B': [2.34, 5.82, 0.1],'C': ['bar', 'baz', 'quux']},
+                     'blue'  : {'A': [3, 2, 4],'B': [2.34, 5.82, 0.1],'C': ['bar', 'baz', 'quux']} }
 
         # spins up N tasks, in this case 3, each with a different set of
         # variable values
-        self.services.run_ensemble(component, ensemble_variables)
+        self.services.run_ensemble(template, variables, run_dir)
