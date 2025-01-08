699738e5860bf505bf7d23ff7322c8f81d737a5f
Mark Coletti <colettima@ornl.gov>
2025-01-08 10:41:52 -0500

    Added init and finalize NOPs. Now properly get config params.

--- doc/examples/ensembles/driver.py
+++ doc/examples/ensembles/driver.py
@@ -15,16 +15,22 @@ class ensemble_driver(Component):
 
     def __init__(self, services, config):
         super().__init__(services, config)
+        print('Creating driver')
+
+    def init(self, timeStamp=0.0):
+        return
 
     def step(self, timestamp=0.0, **keywords):
         """ set up and run the ensemble
         """
         # ENSEMBLE_DIR is an arbitrary variable denoting where we want to run
-        # all the ensembles.
-        run_dir = self.config.get_global_param(self, self.services,
-                                               'ENSEMBLE_DIR')
+        # all the ensembles.  You don't have to use ENSEMBLE_DIR. You can
+        # even hardcode the path here.  Whatever.
+        run_dir = self.services.get_config_param('ENSEMBLE_DIR')
+        print(f'Running ensemble in {run_dir}')
 
-        template = self.config.get_config_param('TEMPLATE')
+        template = self.config['TEMPLATE']
+        print(f'Using template {template}')
 
         # Specifies different sets of variable values for concurrent ensemble
         # runs for two different components, 'a_sim' and 'another_sim',
@@ -35,13 +41,19 @@ class ensemble_driver(Component):
         # same number of variable values, but those within each component
         # there should be the same number.  E.g., 'a_sim' has 3 values for
         # 'A', 'B', and 'C', while 'another_sim' has 2 values for 'D', 'B', and 'F'.
+
         variables = {'a_sim': {'A': [3, 2, 4],
-                                'B': [2.34, 5.82, 0.1],
-                                'C': ['bar', 'baz', 'quux']},
+                               'B': [2.34, 5.82, 0.1],
+                               'C': ['bar', 'baz', 'quux']},
                      'another_sim': {'D': [7, 5],
-                                'B': [0.775, 0.080],
-                                'F': ['xyzzy', 'plud']} }
+                                     'B': [0.775, 0.080],
+                                     'F': ['xyzzy', 'plud']}}
 
         # Spins up N tasks, in this case 5 (3 for a_sim and 2 for blue),
         # each with a different set of variable values.
         # self.services.run_ensemble(template, variables, run_dir)
+
+
+    def finalize(self, timeStamp=0.0):
+        return
+
